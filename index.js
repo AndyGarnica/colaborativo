dcaebdeb01922dd60e5f6b0ea6b7ca5d2db22a19
NatashaDianeey <ndianeey@gmail.com>
2019-02-11 21:26:12 -0600

    se agrega parte de historia del nudo

--- index.js
+++ index.js
@@ -13,12 +13,3 @@ const  edad = Math.round(Math.random() * 100);
 const  villano = "Rich";
 const  lugar = "En far far away";
 const  hobby = "programar";
-
-
-console.log(intro.historia(personaje, lugar, edad));
-
-console.log(cowsay.say({
-  text : 'Hola',
-  e : "O O",
-  T : "U"
-}));
---
+++ nudo.js
@@ -0,0 +1,5 @@
+module.exports = {
+    historia: function (personaje, personaje2, personaje3, lugar, villano) {
+        `${personaje}, ${personaje2}, ${personaje3} y ${personaje} caminaban hambrientos vagando por el bosque ${lugar} sin rumbo, sin pensar que encontrarían a ${villano}, que les ofrecia vida eterna a cambio de que nadaran en el lago, lo que les parecio buena idea que hasta olvidaron el hambre que tenian, ya adentro del agua todos los amigos recordaron el hambre que tenian y comieron pescados crudos dentro del lago. `        
+    }
+}
