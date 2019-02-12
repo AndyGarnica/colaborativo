b77055c734f46e6494de2525ddba4aae4fdc847c
Varonss <martin.varona.84@gmail.com>
2019-02-11 21:42:20 -0600

    creacion de ramdom historia

---
+++ final.js
@@ -0,0 +1,48 @@
+var final = require('./final.js');
+var index = require('./index.js');
+
+module.exports = {
+  historia: function(personaje, personaje2, personaje3, personaje4, edad, villano, lugar, hobby){
+    return `Introducción: ${personaje}  limpiaba los muebles de la casa del bosque cuando Richie llega a pedirle un café.
+ ${personaje4}: Buenos días Andy, me regalas un café por favor.
+${personaje}: Buenos días ${personaje4}, te traeré un té, la cafeína hace daño.
+ ${personaje4}: ${personaje}, no quiero té, un café de cuates.
+${personaje}: Por eso, pero como el café es dañino para la salud, yo te traeré un té, es más saludable. No se hable más, ya vengo con su té.
+ ${personaje4} (molestándose): ${personaje}  no quiero un té, ve a la cocina y tráeme un café.
+${personaje} (haciéndose la ofendida): Ok, no se altere, le traigo su café, pero cuando empiece a sentirse mal, no diga que no se lo advertí. Uno aquí trata de ayudar pero…
+${personaje4} (interrumpiendo molesto): ${personaje} ¡el café!
+${personaje}: Ya voy, ya voy, que falta de paciencia.
+${personaje} sale a buscar el café y Felicidad entra aun sacando la pereza de su cuerpo y bostezando.
+${personaje2}: Buenos días ¿Cómo dormiste?
+ ${personaje4} (irónico): No muy bien roncaste toda la noche.
+${personaje2} (ofendida): ¿Yo roncando? Orlando pero si yo no ronco.
+${personaje}  entra con el café en la mano y se lo entrega a ${personaje4} sin ningún cuidado para meterse en la conversación.
+${personaje}: ¿Qué no ronca dice? amiga si duermes en ese cuarto y uno cree que hay un oso dentro.
+${personaje2}: ${personaje} ¿A ti quien te preguntó?
+${personaje}: Nadie, pero como soy testigo, opinó
+${personaje4}: ${personaje} ¿por qué no vas a ver si estacione bien el auto? y si no es así, ponlo en un buen lugar.
+${personaje}: uuuy, yo no sé manejar
+${personaje4}: No importa, has lo que te digo chingue su.
+${personaje} (caminando hacia la salida): No agradecen, uno los quiere como si son de su familia y ellos mal agradecidos, no agradecen.
+${personaje}  sale del lugar y felicidad toma la taza de café de ${personaje4} y comienza a tomársela, mientras este la mira sin parpadear.
+${personaje2}: ¿Y ${personaje3}  aún no se ha levantado?
+${personaje4}: No, ${personaje3}  siempre ha sido muy perezosa, pero últimamente duerme mucho más que de costumbre.
+${personaje2}: ¿Estará enferma?
+En ese momento aparece ${personaje3}
+${personaje3} (lanzándose en un sofá con cara de sueño): ¿preguntaban por mí?
+${personaje2}: nena ¿estas enferma? Duermes mucho.
+${personaje3}: No we no estoy enferma, es normal, estoy cansada.
+${personaje} que había estado escuchando entra de nuevo.
+${personaje}: Claro que es normal, en su estado se duerme mucho.
+ ${personaje4}: ¿Cuál estado?
+${personaje}: Ah ¿pero es que no saben? viene desde ${lugar}
+${personaje3} (Algo nerviosa): ${personaje} cierra la boca que nadie está hablando contigo.
+${personaje2}: ${personaje4} te pregunto ¿qué estado, ${personaje}?
+${personaje}: Señorita ${personaje3}  y ¿para cuando va a dejar la noticia?  ellos  tienen derecho a saber que serán tios.
+ ${personaje4} y ${personaje2} (a la vez): ¡¿Qué seremos qué?!
+Vick se levanta del sofá y sale rápidamente, mientras sus amigos  van detrás de ella ordenándole que se detenga y explique, ${personaje} se queda sola en el bosque hablando en voz alta.
+${personaje}: A mí no me gusta el chisme, ni meterme donde no me llaman, soy muy discreta, pero esa niña tenía que hablar.
+FIN`
+
+  }
+}
