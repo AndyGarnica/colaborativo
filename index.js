81502bada6ac702080e8c37289af1a5b464fbc82
AndyGarey <elsa.garnica01@gmail.com>
2019-02-12 20:01:09 -0600

    creando rama y modificando codigo

--- index.js
+++ index.js
@@ -13,5 +13,10 @@ const  villano = "Rich";
 const  lugar = "En far far away";
 const  hobby = "programar";
 
+const inicioHistoria = inicio.historia(personaje, personaje2, personaje3, personaje4, hobby);
+const nudoHistoria = nudo.historia(personaje,personaje2, personaje3, personaje4,lugar,villano,hobby, edad);
+const finalHistoria = final.historia(personaje,personaje2, personaje3, personaje4,lugar,villano,hobby, edad);
 
-console.log(inicio.historia(personaje, personaje2, personaje3, personaje4, hobby), nudo.historia(personaje,personaje2, personaje3, personaje4,lugar,villano,hobby, edad), final.historia(personaje,personaje2, personaje3, personaje4,lugar,villano,hobby, edad));
+const historiaCompleta= inicioHistoria + nudoHistoria + finalHistoria
+
+console.log(historiaCompleta); 
